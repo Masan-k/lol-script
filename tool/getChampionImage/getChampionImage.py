8bd77a91f6f19938b920738aec631d7cf80d8f14
Masan-k <uronti@hotmail.co.jp>
2025-12-07 19:24:57 +0900

    Remove output images from repo

--- tool/getChampionImage/getChampionImage.py
+++ tool/getChampionImage/getChampionImage.py
@@ -1,15 +1,25 @@
 import os,json,requests
 json_open = open('input/champion.json','r');
 json_load = json.load(json_open);
-base_url = 'https://ddragon.leagueoflegends.com/cdn/15.24.1/img/champion/'
-save_dir = 'output'
+save_dir_square = 'output/square'
+save_dir_splash = 'output/splash'
+base_url_square = 'https://ddragon.leagueoflegends.com/cdn/15.24.1/img/champion/'
+base_url_splash = 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'
+
+os.makedirs(save_dir_square, exist_ok=True)
+os.makedirs(save_dir_splash, exist_ok=True)
 
 for v in json_load['data'].values():
+  #file_name = v['id'] + '_0.jpg'
+  #save_path_splash = os.path.join(save_dir_splash,file_name)
+  #download_url_splash = base_url_splash + file_name 
+  #res = requests.get(download_url_splash).content
+
   file_name = v['id'] + '.png'
-  
-  save_path = os.path.join(save_dir,file_name)
-  download_url = base_url + file_name 
-  res = requests.get(download_url).content
+  save_path_square = os.path.join(save_dir_square,file_name)
+  download_url_square = base_url_square + file_name 
+  res = requests.get(download_url_square).content
  
-  with open(save_path, mode='wb') as f:
-    f.write(res)
+  with open(save_path_square, mode='wb') as f:
+    f.write(res) 
+
